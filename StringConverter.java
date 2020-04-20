8d822a5ccd30d907540b73ce2c2244527275b5e5
cfujitani20 <cfujitani20@punahou.edu>
2020-04-20 04:09:44 +0000

    Added method headers

---
+++ StringConverter.java
@@ -0,0 +1,23 @@
+public class StringConverter
+{
+    // reverse string method
+    public static String reverse(String str)
+    {
+
+    }
+    // check if word is a palindrome method
+    public static boolean checkPalindrome(String str)
+    {
+
+    }
+    // convert the string word into pig latin 
+    public static String pigLatinate(String str)
+    {
+
+    }
+    // converts the string into shorthand version 
+    public static String shorthand(String str)
+    {
+
+    }
+}
